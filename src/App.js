87ed6f33b2660e077d9820f5ea23be7192019422
Ketan <ketantoo@gmail.com>
2020-04-29 16:13:31 +0530

    list rendering done

--- src/App.js
+++ src/App.js
@@ -1,21 +1,50 @@
 import React from "react";
 
-const welcome = {
-  greeting: "Hey",
-  title: "React",
-};
+const list = [
+  {
+    title: "React",
+    url: "https://reactjs.org/",
+    author: "Jordan Walke",
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: "Redux",
+    url: "https://redux.js.org/",
+    author: "Dan Abramov, Andrew Clark",
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
 
 function getTitle(title) {
   return title;
- }
-function App() { 
+}
+function App() {
   // const title = "React";
 
-  return (<div>
-    <h1>{welcome.greeting} {getTitle("React!!")} </h1>
-    <label htmlFor="search">Search: </label>
-    <input id="search" type="text"/>
-  </div>);
+  return (
+    <div>
+      <h1>My Hacker Stories</h1>
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" />
+      <hr />
+      {list.map(function (item) {
+        return (
+          <div key={item.objectID}>
+            <span>
+              <a href={item.url}>{item.title}</a>
+            </span>
+            <span> {item.author}</span>
+            <span>{item.num_comments}</span>
+            <span>{item.points}</span>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
 export default App;
