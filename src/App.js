682a978e3db83e9886b7513be9c1ab5b9d83c1d6
Ketan <ketantoo@gmail.com>
2020-04-30 21:31:46 +0530

    es6 arrow functions

--- src/App.js
+++ src/App.js
@@ -22,18 +22,16 @@ const list = [
 function List() {
   return (
     <div>
-      {list.map(function (item) {
-        return (
-          <div key={item.objectID}>
-            <span>
-              <a href={item.url}>{item.title}</a>
-            </span>
-            <span> {item.author}</span>
-            <span>{item.num_comments}</span>
-            <span>{item.points}</span>
-          </div>
-        );
-      })}
+      {list.map((item) => (
+        <div key={item.objectID}>
+          <span>
+            <a href={item.url}>{item.title}</a>
+          </span>
+          <span> {item.author}</span>
+          <span>{item.num_comments}</span>
+          <span>{item.points}</span>
+        </div>
+      ))}
     </div>
   );
 }
