9979b837589b16389d5e15b66d328b6beddbc12f
Ketan <ketantoo@gmail.com>
2020-05-01 21:32:52 +0530

    ES6 class

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 
 const list = [
   {
@@ -35,18 +35,38 @@ function List() {
     </div>
   );
 }
-function App() {
+class App extends Component {
   // const title = "React";
+  constructor(props) {
+    super(props);
+    this.state = {
+      list: list,
+    };
+  }
 
-  return (
-    <div>
-      <h1>My Hacker Stories</h1>
-      <label htmlFor="search">Search: </label>
-      <input id="search" type="text" />
-      <hr />
-      <List />
-    </div>
-  );
+  render() {
+    return (
+      // <div>
+      //   <h1>My Hacker Stories</h1>
+      //   <label htmlFor="search">Search: </label>
+      //   <input id="search" type="text" />
+      //   <hr />
+      //   <List />
+      // </div>
+      <div className="App">
+        {this.state.list.map((item) => (
+          <div key={item.objectID}>
+            <span>
+              <a href={item.url}>{item.title}</a>
+            </span>
+            <span>{item.author}</span>
+            <span>{item.num_comments}</span>
+            <span>{item.points}</span>
+          </div>
+        ))}
+      </div>
+    );
+  }
 }
 
 export default App;
