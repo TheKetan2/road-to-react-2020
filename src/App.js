a99343def5d660b77c167529732e9e1c5f42d5b9
Ketan <ketantoo@gmail.com>
2020-05-03 14:27:10 +0530

    Spliting component

--- src/App.js
+++ src/App.js
@@ -73,7 +73,14 @@ class App extends Component {
       //   <List />
       // </div>
       <div className="App">
-        <form>
+        <Search value={searchTerm} onChange={this.onSearchChange} />
+        <Table
+          list={list}
+          pattern={searchTerm}
+          onDissmis={this.onDismiss}
+          isSearched={this.isSearched}
+        />
+        {/* <form>
           <input
             type="text"
             value={searchTerm}
@@ -101,12 +108,47 @@ class App extends Component {
                 </button>
               </span>
             </div>
-          ))}
+          ))} */}
       </div>
     );
   }
 }
 
+class Search extends Component {
+  render() {
+    const { value, onChange } = this.props;
+    return (
+      <form>
+        <input type="text" value={value} onChange={onChange} />
+      </form>
+    );
+  }
+}
+
+class Table extends Component {
+  render() {
+    const { list, pattern, onDismiss, isSearched } = this.props;
+    return (
+      <div>
+        {list.filter(isSearched(pattern)).map((item) => (
+          <div key={item.objectID}>
+            <span>
+              <a href={item.url}>{item.title}</a>
+            </span>
+            <span>{item.author}</span>
+            <span>{item.num_comments}</span>
+            <span>{item.points}</span>
+            <span>
+              <button onClick={() => onDismiss(item.objectID)} type="button">
+                Dismiss
+              </button>
+            </span>
+          </div>
+        ))}
+      </div>
+    );
+  }
+}
 export default App;
 
 // Page No: 76 Split Up Components
