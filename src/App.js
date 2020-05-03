02a7f978299a837d0bb3b0f73545f24a9ab0f791
Ketan <ketantoo@gmail.com>
2020-05-03 20:21:51 +0530

    Component Declaration

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-
+import "./App.css";
 const list = [
   {
     title: "React",
@@ -19,22 +19,6 @@ const list = [
   },
 ];
 
-function List() {
-  return (
-    <div>
-      {list.map((item) => (
-        <div key={item.objectID}>
-          <span>
-            <a href={item.url}>{item.title}</a>
-          </span>
-          <span> {item.author}</span>
-          <span>{item.num_comments}</span>
-          <span>{item.points}</span>
-        </div>
-      ))}
-    </div>
-  );
-}
 class App extends Component {
   // const title = "React";
   constructor(props) {
@@ -65,94 +49,53 @@ class App extends Component {
   render() {
     const { searchTerm, list } = this.state;
     return (
-      // <div>
-      //   <h1>My Hacker Stories</h1>
-      //   <label htmlFor="search">Search: </label>
-      //   <input id="search" type="text" />
-      //   <hr />
-      //   <List />
-      // </div>
-      <div className="App">
-        <Search value={searchTerm} onChange={this.onSearchChange}>
-          Search
-        </Search>
+      <div className="page">
+        <div className="interactions">
+          <Search value={searchTerm} onChange={this.onSearchChange}>
+            Search
+          </Search>
+        </div>
+
         <Table
           list={list}
           pattern={searchTerm}
           onDismiss={this.onDismiss}
           isSearched={this.isSearched}
         />
-        {/* <form>
-          <input
-            type="text"
-            value={searchTerm}
-            onChange={this.onSearchChange}
-          />
-        </form>
-        {list
-          .filter((item) =>
-            item.title.toLowerCase().includes(searchTerm.toLowerCase())
-          )
-          .map((item) => (
-            <div key={item.objectID}>
-              <span>
-                <a href={item.url}>{item.title}</a>
-              </span>
-              <span>{item.author}</span>
-              <span>{item.num_comments}</span>
-              <span>{item.points}</span>
-              <span>
-                <button
-                  onClick={() => this.onDismiss(item.objectID)}
-                  type="button"
-                >
-                  Dismiss
-                </button>
-              </span>
-            </div>
-          ))} */}
       </div>
     );
   }
 }
 
-class Search extends Component {
-  render() {
-    const { value, onChange, children } = this.props;
-    return (
-      <form>
-        {children}
-        <input type="text" value={value} onChange={onChange} />
-      </form>
-    );
-  }
-}
+const Search = ({ value, onChange, children }) => (
+  <form>
+    {children}
+    <input type="text" value={value} onChange={onChange} />
+  </form>
+);
 
-class Table extends Component {
-  render() {
-    const { list, pattern, onDismiss, isSearched, children } = this.props;
-    return (
-      <div>
-        {list.filter(isSearched(pattern)).map((item) => (
-          <div key={item.objectID}>
-            <span>
-              <a href={item.url}>{item.title}</a>
-            </span>
-            <span>{item.author}</span>
-            <span>{item.num_comments}</span>
-            <span>{item.points}</span>
-            <span>
-              <Button onClick={() => onDismiss(item.objectID)}>Dismiss</Button>
-              {/* <button onClick={() => onDismiss(item.objectID)} type="button">
-                Dismiss
-              </button> */}
-            </span>
-          </div>
-        ))}
+const Table = ({ list, pattern, onDismiss, isSearched, children }) => (
+  <div className="table">
+    {list.filter(isSearched(pattern)).map((item) => (
+      <div key={item.objectID} className="table-row">
+        <span style={{ width: "40%" }}>
+          <a href={item.url}>{item.title}</a>
+        </span>
+        <span style={{ width: "30%" }}>{item.author}</span>
+        <span style={{ width: "10%" }}>{item.num_comments}</span>
+        <span style={{ width: "10%" }}>{item.points}</span>
+        <span style={{ width: "10%" }}>
+          <Button
+            onClick={() => onDismiss(item.objectID)}
+            className="button-inline"
+          >
+            Dismiss
+          </Button>
+        </span>
       </div>
-    );
-  }
-}
+    ))}
+  </div>
+);
 
 class Button extends Component {
   render() {
