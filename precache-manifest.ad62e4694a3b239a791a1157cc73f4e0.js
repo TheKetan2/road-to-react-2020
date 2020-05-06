124e2223cb535af544fba9c4776ebcf7039df919
Ketan <ketantoo@gmail.com>
2020-05-06 15:50:26 +0530

    Updates

---
+++ precache-manifest.ad62e4694a3b239a791a1157cc73f4e0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "412ff92e7aba74ae74212188cf641a21",
+    "url": "/road-to-react-2020/index.html"
+  },
+  {
+    "revision": "432c468b9e4dc635d547",
+    "url": "/road-to-react-2020/static/css/main.0c4e38fc.chunk.css"
+  },
+  {
+    "revision": "118c7e5be5ee91ac0951",
+    "url": "/road-to-react-2020/static/js/2.a1b18ed4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/road-to-react-2020/static/js/2.a1b18ed4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "432c468b9e4dc635d547",
+    "url": "/road-to-react-2020/static/js/main.60af2b1c.chunk.js"
+  },
+  {
+    "revision": "1c4c2d1228f9f4048596",
+    "url": "/road-to-react-2020/static/js/runtime-main.5451963a.js"
+  }
+]);
